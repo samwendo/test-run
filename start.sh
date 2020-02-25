a16c1e43e9ac63c0da958f5aff7ee4f4f8aace64
samwendo <wendosam21@gmail.com>
2020-02-25 11:01:17 +0300

    deploying to heroku

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export SECRET_KEY='butterfly56'
-export MAIL_USERNAME='wendosam21@gmail.com'
-export MAIL_PASSWORD='samwendo'
+export MAIL_USERNAME='wendosam82@gmail.com'
+export MAIL_PASSWORD='0718202998'
 export DATABASE_URL='postgresql+psycopg2://sam:123@localhost/reblo'
 
 python3.6 manage.py server
